5d7e45eab1c509fb0cc30a45bda8180d3b86a37f
Hevar <39927976+hevar333@users.noreply.github.com>
2025-06-30 17:12:33 +0300

    Update language-selector.js

--- language-selector.js
+++ language-selector.js
@@ -43,13 +43,13 @@ const LanguageSelector = (() => {
     const path = window.location.pathname.toLowerCase();
 
     if (path.includes('./english1.html')) {
-      currentLangSpan.textContent = 'English';
+      currentLangSpan.textContent = 'english';
     } else if (path.includes('./sorani1.html')) {
-      currentLangSpan.textContent = 'Kurdish (Sorani)';
+      currentLangSpan.textContent = 'kurdish (Sorani)';
     } else if (path.includes('./kurdishku.html')) {
-      currentLangSpan.textContent = 'Kurdish (Kurmanji)';
+      currentLangSpan.textContent = 'kurdish (Kurmanji)';
     } else if (path.includes('./arabic1.html')) {
-      currentLangSpan.textContent = 'Arabic';
+      currentLangSpan.textContent = 'arabic';
     } else {
       currentLangSpan.textContent = 'Select Language';
     }
