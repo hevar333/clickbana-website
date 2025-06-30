d6a2c2eb599e47529018c4ca6fa7dbbe16f29dde
Hevar <39927976+hevar333@users.noreply.github.com>
2025-06-30 16:33:33 +0300

    Update kurdishjs.js

--- kurdishjs.js
+++ kurdishjs.js
@@ -1,7 +1,7 @@
-import { setupKurdishKeyboardMapping } from '/clickbana-website/kurdishKeyboardMapping.js';
-import { emojiCategories, populateEmojiPanel } from '/clickbana-website/emoji-data.js';
-import { LanguageSelector } from '/clickbana-website/language-selector.js';
-import initContextMenuSearch from '/clickbana-website/contextMenuSearch.js';
+import { setupKurdishKeyboardMapping } from 'kurdishKeyboardMapping.js';
+import { emojiCategories, populateEmojiPanel } from 'emoji-data.js';
+import { LanguageSelector } from 'language-selector.js';
+import initContextMenuSearch from 'contextMenuSearch.js';
 
   const editor = document.getElementById('text-input');
   const wordCountEl = document.getElementById('wordCount');
